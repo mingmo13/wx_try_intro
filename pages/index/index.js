06080acbc5e9fa53cf9f660d088fa296cbd0d5b4
Raina <764883754@qq.com>
2016-10-26 16:31:53 +0800

    Add files via upload

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,1 +1,41 @@
-
+//index.js
+//获取应用实例
+var app = getApp()
+Page({
+  data: {
+    motto: 'Hello World',
+    userInfo: {}
+  },
+  //事件处理函数
+  bindViewTap: function() {
+    wx.navigateTo({
+      url: '../logs/logs'
+    })
+  },
+  gotoNote: function() {
+    wx.navigateTo({
+      url: '../note/note'
+    })
+  },
+  gotoLove: function() {
+    wx.navigateTo({
+      url: '../love/love'
+    })
+  },
+  gotoEmo: function() {
+    wx.navigateTo({
+      url: '../emo/emo'
+    })
+  },
+  onLoad: function () {
+    console.log('onLoad')
+    var that = this
+    //调用应用实例的方法获取全局数据
+    app.getUserInfo(function(userInfo){
+      //更新数据
+      that.setData({
+        userInfo:userInfo
+      })
+    })
+  }
+})
